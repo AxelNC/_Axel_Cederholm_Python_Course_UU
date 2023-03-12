a4fc8bd0290a8da2a0bb58cbbbd2db23e3eb28f1
Axel Cederholm <axel.cederholm@imbim.uu.se>
2023-03-12 19:02:03 +0100

    Uploaded_ex_day_1_and_ex_day_2_Axel_Cederholm

---
+++ test_animals.py
@@ -0,0 +1,11 @@
+# Import classes from your brand new package 
+from Test_animals_packagde_day_2 import mammals
+from Test_animals_packagde_day_2 import birds
+  
+# Create an object of Mammals class & call a method of it 
+myMammal = mammals.Mammals() 
+myMammal.printMembers() 
+  
+# Create an object of Birds class & call a method of it 
+myBird = birds.Birds() 
+myBird.printMembers()
